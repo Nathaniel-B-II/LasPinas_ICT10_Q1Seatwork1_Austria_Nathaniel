88313f0ef3009751e0af3589c3581f8233c93afd
Nathaniel Nicolas B. Austria <austria.nb@obmontessori.edu.ph>
2026-09-04 07:38:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+studname = "Ehan Castaneda"
+studage = 14 #______
+stud_height = 139.7 #______
+country_to_visit = ["Japan", "USA", "Israel"] #______
+stud_new = False #______
+stud_info = {
+    "color": "azure_blue", 
+    "car_brand": "Tesla", 
+     "shoe_size": 8, 
+     "best_friend": "Nick"
+     } #______
+stud_favefruits =  {
+    "Banana",
+    "Apple",
+    "Cherry"
+} #______
+
+days_of_week = {
+    "Monday"
+    "Tuesday"
+    "Wednesday"
+    "Thursday"
+    "Friday"
+    "Saturday"
+    "Sunday"
+} #______
+
+print("Name:", studname)
+print("Age:", studage)
+print("Height:", stud_height)
